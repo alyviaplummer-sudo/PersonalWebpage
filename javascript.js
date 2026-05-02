6f07bf1e31e56ce766a2f61367fa2ddbc198ef56
alyviaplummer-sudo <alyvia.plummer@gmail.com>
2026-05-02 01:28:45 -0500

    adding node_module stuff

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+import { gsap } from "\gsap";
+import { ScrollTrigger } from "\gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 
-
+gsap.to(".footer", {
+  duration: 3,
+  rotation: 360,
+  scale: 2,
+  scrollTrigger: {
+    trigger: ".footer",
+    markers: true,
+    scrub: true,
+  },
+});
