b4c11c85dbdf4aebe00d4783030d015e7af1ea04
alyviaplummer-sudo <alyvia.plummer@gmail.com>
2026-05-03 22:26:19 -0500

    added gsap tween

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
-import { gsap } from "\gsap";
-import { ScrollTrigger } from "\gsap/ScrollTrigger";
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger, SplitText);
+});
 
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.to(".footer", {
-  duration: 3,
+gsap.to(".mephoto", {
+  scrollTrigger: ".mephoto",
   rotation: 360,
-  scale: 2,
-  scrollTrigger: {
-    trigger: ".footer",
-    markers: true,
-    scrub: true,
-  },
+  duration: 3,
+});
+
+let split = SplitText.create(".headerheader", {
+  type: "chars,words, lines",
+  wordsClass: "lines, chars",
+  smartWrap: true,
+});
+
+gsap.from(split.chars, {
+  y: 100,
+  autoAlpha: 0,
+  stagger: 0.05,
+  yoyo: true,
 });
